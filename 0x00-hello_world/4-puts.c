7fffe6d8194f4f323460728f1581456285c20971
billyotieno <billybrian94@gmail.com>
2022-04-07 03:02:05 -0700

    added gcc compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* This is the main entry point */
 	char string[300] = "\"Programming is like building a multilingual puzzle";
+
 	puts(string);
 	return (0);
 }
