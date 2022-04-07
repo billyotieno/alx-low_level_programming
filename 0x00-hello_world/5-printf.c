d57004fca9e93fca0736c21a23a3643eb87a1a05
billyotieno <billybrian94@gmail.com>
2022-04-07 03:22:11 -0700

    made changes to printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-/* This is the main entry point */
-int main(void){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 
 }
 
