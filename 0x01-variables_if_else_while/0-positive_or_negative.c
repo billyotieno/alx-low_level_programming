8c10e7b554a01e046188e75ba32a03898fca4f5a
billyotieno <billybrian94@gmail.com>
2022-04-08 12:58:55 -0700

    Fixed positive or negative c file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,18 +13,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n % 10;
-	if (l > 5)
+	
+	if (n > 0)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+		printf("is positive");
 	}
-	else if (l == 0)
+	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, l);
+		printf("is zero");
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+		printf("is negative");
 	}
 	return (0);
 }
