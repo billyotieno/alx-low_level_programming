052399008c8b015b35779246c980ad715d37bf4a
billyotieno <billybrian94@gmail.com>
2022-04-27 12:06:48 -0700

    added code to add to memoery

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - Fills memory with a constant byte
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (n > 0)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+
+	return (s);
+}
