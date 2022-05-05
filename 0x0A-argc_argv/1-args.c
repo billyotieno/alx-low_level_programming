25a47f2a768f307e4e44fadf9e7791950811f777
billyotieno <billybrian94@gmail.com>
2022-05-05 14:31:08 -0700

    Added betty syntax for 1 and 4

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
- * main - function main
- * This program prints the number of arguments passed to it.
- * main - print the number of args passed to it
- * @argc - argument count
- * @argv - argument vector/array of strings
+/**
+ * main - print the number of args passed to it.
+ * @argc: argument count.
+ * @argv: argument vector/array of strings.
+ * Description: description for the main function.
+ * Return: 0.
  */
 
 int main(int argc, char **argv)
