25b2aa0013f5dc349ab216c2d3dad97c6113ce70
billyotieno <billybrian94@gmail.com>
2022-05-10 16:53:51 -0700

    fix: made changes to headerfile

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,6 @@
-#include <stdlib.h>
+#ifdef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -6,3 +8,5 @@ int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
