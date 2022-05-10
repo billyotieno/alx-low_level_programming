82075e52daa51e404c2ffef990b3caf21ee57a59
billyotieno <billybrian94@gmail.com>
2022-05-10 16:24:37 -0700

    add: calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an [] given number of elements and size
+ * @nmemb: no. of elements
+ * @size: size of each element
+ * Return: returns pointer allocated to memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *pnt;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	pnt = malloc(nmemb * size);
+	if (pnt == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		pnt[i] = 0;
+
+	return (pnt);
+}
