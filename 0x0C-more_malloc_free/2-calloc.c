3ec8b650a9555e4970fbee1ed1e824b44d466018
billyotieno <billybrian94@gmail.com>
2022-05-10 17:05:25 -0700

    update: updated calloc script

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,25 +1,24 @@
 #include "main.h"
-
+#include <limits.h>
 /**
- * _calloc - allocates memory for an arr given number of elements and size
- * @nmemb: no. of elements
- * @size: size of each element
- * Return: returns pointer allocated to memory
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: elements of memory needed
+ * @size: size in bytes of each element
+ * Return: pointer to the allocated memory
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *pnt;
 	unsigned int i;
+	char *p;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	pnt = malloc(nmemb * size);
-	if (pnt == NULL)
+	if (size >= UINT_MAX / nmemb || nmemb >= UINT_MAX / size)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		pnt[i] = 0;
-
-	return (pnt);
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+	return ((void *)p);
 }
