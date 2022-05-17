3f169b371b3d8a134164dd11a2f2b1e6d2952f97
billyotieno <billybrian94@gmail.com>
2022-05-17 15:22:40 -0700

    added: script for variadic summation

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+
+/**
+* sum_them_all - function with two arguments
+* @n: first integer argument
+* description: returns the sum of params
+* Return: zero or summation
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list arg;
+
+	if (n == 0)
+		return (0);
+	va_start(arg, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(arg, int);
+	}
+	va_end(arg);
+	return (sum);
+}
