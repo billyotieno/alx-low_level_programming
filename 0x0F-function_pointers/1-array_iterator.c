f942c85668e0afbc0fa088c33b839e88744e84ce
billyotieno <billybrian94@gmail.com>
2022-05-17 14:05:18 -0700

    added: array iterator function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,11 +2,10 @@
 
 /**
   * array_iterator - function that iterates over an array
-  * @array - array of integers
-  * @size - array size
-  * @action - function pointer to an array that takes an integer value returns void
+  * @array: array of integers
+  * @size: array size
+  * @action: function pointer to an array
   */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
