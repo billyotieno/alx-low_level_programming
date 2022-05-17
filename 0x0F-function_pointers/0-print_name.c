6d5cfe20ae5adc7893e2ed7bfde77d15b69342cd
billyotieno <billybrian94@gmail.com>
2022-05-17 13:44:55 -0700

    add: script to print name with func pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - Prints name from a func pointer
+  * @name: character string
+  * @f: function pointer that takes a string character
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
