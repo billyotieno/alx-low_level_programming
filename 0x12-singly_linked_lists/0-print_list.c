6413ac5dbffd070a5d223eed76940d7e0c323647
billyotieno <billybrian94@gmail.com>
2022-05-24 13:23:06 -0700

    added: func to  print all node elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+* print_list - print all elements of `list_t` list
+* @h: head of linked list
+* Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	const list_t *present_node;
+	size_t c;
+
+	c = 0;
+	present_node = h;
+	while (present_node != NULL)
+	{
+		printf("[%d] %s\n", present_node->len, present_node->str);
+		present_node = present_node->next;
+		c++;
+	}
+
+	return (c);
+}
