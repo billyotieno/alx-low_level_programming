8062bc70dacc7c89ad553b8769f19e55556a8bfe
billyotieno <billybrian94@gmail.com>
2022-05-24 14:38:36 -0700

    uploaded

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,35 +1,45 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
+
+int str_recursion(char *s);
 
 /**
- * add_node - add a new node at the beginning of `list_t` list
- * @head: double pointer to head
- * @str: string to be saved in new node, must be duplicated
- * Return: Address of new element or NULL if failed
- */
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: string to set in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *n_node;
-	int c;
+	list_t *new;
 
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
-	n_node = malloc(sizeof(list_t));
-	if (n_node == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		free(n_node);
 		return (NULL);
 	}
-	n_node->str = strdup(str);
-	if (n_node->str == NULL)
+
+	new->str = strdup(str);
+	if (!new->str)
 	{
-		free(n_node);
+		free(new);
 		return (NULL);
 	}
-	n_node->len = c;
-	n_node->next = *head;
+	new->len = str_recursion(new->str);
+	new->next = *head;
+	*head = new;
 
-	*head = n_node;
-	return (*head);
+	return (new);
+}
+
+/**
+ * str_recursion - returns the length of a string.
+ * @s: string.
+ * Return: length of @s.
+ */
+int str_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + str_recursion(s + 1));
 }
