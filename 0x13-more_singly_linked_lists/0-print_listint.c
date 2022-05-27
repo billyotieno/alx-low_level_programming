87425de75df45ee4cf9295d887308a0c28096980
billyotieno <billybrian94@gmail.com>
2022-05-27 14:05:31 -0700

    Updated and corrected Betty highlights

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,7 @@ size_t print_listint(const listint_t *h)
 	const listint_t *c;
 	size_t count;
 
-	for (c = h, count=0; c != NULL; count++, c = c->next)
+	for (c = h, count = 0; c != NULL; count++, c = c->next)
 		printf("%d\n", c->n);
 
 	return (count);
