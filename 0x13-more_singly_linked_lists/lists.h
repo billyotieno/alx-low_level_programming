dcd128873701567c5d34a8f3b2b6fbec66d71c54
billyotieno <billybrian94@gmail.com>
2022-05-27 13:59:18 -0700

    update header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
-
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -31,4 +31,4 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-#endif LIST_H
+#endif
