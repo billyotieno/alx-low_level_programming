d29bb5c9310d5f560d839fe69cb3d1f04e22a5c0
billyotieno <billybrian94@gmail.com>
2022-05-27 14:10:58 -0700

    script to output number of elements in the list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - number of elements in linked_list
+ * @h: linked list head
+ * return: number of elements in linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *d;
+	size_t count;
+
+	for (d = h, count = 0; d != NULL; count++, d = d->next)
+		;
+
+	return (count);
+}
