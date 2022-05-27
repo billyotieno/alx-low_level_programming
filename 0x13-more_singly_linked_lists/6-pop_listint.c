f98ac6d8006662a0c2fb791cd964f9b78cd9d865
billyotieno <billybrian94@gmail.com>
2022-05-27 16:32:41 -0700

    added: solution to remove first node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* pop_listint - deletes the head node for linkedlist
+* @head: double pointer to head node of linked list
+* Return: head nodes data (n)
+*/
+int pop_listint(listint_t **head)
+{
+	int num;
+	listint_t *hold_node;
+
+	if (*head == NULL)
+		return (0);
+
+	hold_node = *head;
+	num = hold_node->n;
+	*head = hold_node->next;
+	free(hold);
+
+	return (num);
+}
+
+
