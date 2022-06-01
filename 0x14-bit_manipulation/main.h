37cba8368d54c84c06e21421cf9ac9a31035e5b6
billyotieno <billybrian94@gmail.com>
2022-05-31 17:11:40 -0700

    added libs to header

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
 int _putchar(char c);
 unsigned int binary_to_unit(const char *b);
 void print_binary(unsigned long int n);
