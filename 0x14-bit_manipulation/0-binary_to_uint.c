ce0f0375cdc02fde1e4c0bf2b8460e47ed810d1a
billyotieno <billybrian94@gmail.com>
2022-05-31 17:25:14 -0700

    updated: file 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,25 +10,22 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num, i;
 
-if (!b || !*b)
-return (0);
+	if (!b || !*b)
+		return (0);
 
-num = i = 0;
-while (b[i])
-{
-if (b[i] > 49)
-return (0);
-else if (b[i] == 49)
-{
-num <<= 1;
-num += 1;
-}
-else
-num <<= 1;
-i++;
-}
-return (num);
-
-
-
+	num = i = 0;
+	while (b[i])
+	{
+		if (b[i] > 49)
+			return (0);
+		else if (b[i] == 49)
+		{
+			num <<= 1;
+			num += 1;
+		}
+		else
+		num <<= 1;
+		i++;
+	}
+	return (num);
 }
