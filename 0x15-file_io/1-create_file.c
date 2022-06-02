12bf1a61896bb97e4cd221a9d47614fb934c0823
billyotieno <billybrian94@gmail.com>
2022-06-02 13:59:03 -0700

    updated: corrected betty styling mistakes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,8 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY
+			| O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_descriptor == -1)
 		return (-1);
 
