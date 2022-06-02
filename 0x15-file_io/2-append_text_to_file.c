a32fcc72fc10e9c7bb3f2a84900b5335d1f75b90
billyotieno <billybrian94@gmail.com>
2022-06-02 14:07:52 -0700

    corrected betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,12 +12,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-		
 	if (text_content == NULL)
 		return (1);
 
 	file_descriptor = open(filename, O_APPEND | O_WRONLY);
-	
 	if (file_descriptor == -1)
 		return (-1);
 
