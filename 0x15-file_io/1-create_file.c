49ad1c052e62baf7b70ffa457f1fb8e96ca9a2fc
billyotieno <billybrian94@gmail.com>
2022-06-02 13:56:32 -0700

    added: solution to create file task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - function creating a file with read/write
+ * @filename: file name
+ * @text_content: string to write to file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor;
+	int status;
+	int i;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		status = write(file_descriptor, text_content, i);
+		if (status == -1)
+			return (-1);
+	}
+
+	close(file_descriptor);
+	return (1);
+}
