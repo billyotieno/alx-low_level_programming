c7be9512527050f9d00e981acbcae65c55d77db5
billyotieno <billybrian94@gmail.com>
2022-06-11 12:12:56 -0700

    added is lower func

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _islower - function checks if character is lower
+* @c: character to be evaluated
+* Return: 1 if upper, 0 if lower
+*/
+int _islower(int c)
+{
+
+if (c >= 'A' && c <= 'B')
+{
+return (0);
+}
+else
+{
+return (1);
+}
+
+}
