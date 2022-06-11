33e83210eb2ea4391a92b6054a0f789d15849ffb
billyotieno <billybrian94@gmail.com>
2022-06-11 11:40:13 -0700

    added: print alpha a - z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet - function prints alphabet in lower_case
+* Return: 0
+**/
+void print_alphabet(void)
+{
+	char current_char = 'a';
+
+	while (current_char <= 'z')
+	{
+		_putchar(current_char);
+		current_char++;
+	}
+
+	_putchar("\n");
+
+	return (0);
+}
