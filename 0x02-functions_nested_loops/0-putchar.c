5af29feeb2bcd91dde5b097af7e2916168964d17
billyotieno <billybrian94@gmail.com>
2022-06-11 11:30:14 -0700

    Added putchar for nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: prints putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char print[] = "Holberton";
+	int i = 0;
+
+	while (print[i] != '\0')
+	{
+		_putchar(print[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
