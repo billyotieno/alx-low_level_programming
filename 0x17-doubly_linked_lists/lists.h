a17ac120989c60dd9577c976337e143dbb64bd93
Billy Otieno <billybrian94@gmail.com>
2022-07-08 23:17:50 +0300

    updated: lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,7 @@
-#ifndef DDL
-#define DDL
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -16,9 +18,6 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
-
-#include <stdio.h>
-#include <stdlib.h>
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -38,6 +37,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 int is_palind(void);
 void find_palind(void);
-
 
 #endif
