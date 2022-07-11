2bddc0ff4f3c741cd96454035a89ea3554800fb4
Billy Otieno <billybrian94@gmail.com>
2022-07-12 00:00:18 +0300

    added: scripts related to dynamic libraries

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("Holberton"));
+	return (EXIT_SUCCESS);
+}
